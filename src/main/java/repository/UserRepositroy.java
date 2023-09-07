9d2f02cfa6b4ad38293dd32d9c6c5c0d5a623cc4
farzadafi <farzadafi40@gmail.com>
2023-09-07 10:49:40 +0330

    add save method

--- src/main/java/repository/UserRepositroy.java
+++ src/main/java/repository/UserRepositroy.java
@@ -1,4 +1,28 @@
 package repository;
 
+import connection.JdbcConnection;
+import model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class UserRepositroy {
+
+    JdbcConnection jdbcConnection = new JdbcConnection();
+    Connection connection = jdbcConnection.getConnection();
+
+    public UserRepositroy() throws SQLException {
+    }
+
+    public int save(User user) throws SQLException {
+        String add = "INSERT INTO users(firstname, lastname, username, password) VALUES (?, ?, ?, ?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(add);
+        preparedStatement.setString(1, user.getFirstname());
+        preparedStatement.setString(2, user.getLastname());
+        preparedStatement.setString(3, user.getUsername());
+        preparedStatement.setString(4, user.getPassword());
+        int result = preparedStatement.executeUpdate();
+        return result;
+    }
 }
