e1878f8aa050dda94d91e0aa6d35eb64b2ea3081
farzadafi <farzadafi40@gmail.com>
2023-09-07 10:33:09 +0330

    add command for create users table

---
+++ src/main/java/sql/table.sql
@@ -0,0 +1,7 @@
+CREATE  TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL
+)
